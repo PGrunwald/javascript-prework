8a5fd5b5be4d35c2e3ff3f1380888a35eac9b097
Paulina Grunwald <grunwald.paulinaa@gmail.com>
2021-08-28 12:48:01 +0200

    Add move variables

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, playerMove;
+computerMove = 'kamień';
+playerMove= 'papier';
+printMessage('Zagrałem'+ computerMove + '! Jeśli Twój ruch to papier, to wygrywasz ');
+printMessage('Zagrałeś'+ playerMove + '! Wygrywasz ');
