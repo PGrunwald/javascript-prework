cc6f01c336623f2b5a4e1498817ae27c485c141d
Paulina Grunwald <grunwald.paulinaa@gmail.com>
2021-08-28 13:21:02 +0200

    Added player move

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
-var computerMove, randomNumber, playerMove;
+var computerMove, randomNumber;
 randomNumber=Math.floor(Math.random() * 3 +1);
 
 console.log('wylosowana liczba to: ' + randomNumber);
 
 if(randomNumber=='1'){
-    computerMove='kamień'
+    computerMove='kamień';
 } else if (randomNumber=='2'){
-    computerMove='papier'
+    computerMove='papier';
 } else if (randomNumber=='3'){
-    computerMove='nożyce'
+    computerMove='nożyce';
 } else{
     computerMove='nieznany ruch';
 }
 
 printMessage('Mój ruch'+ computerMove);
 
+
+var playerMove, playerInput;
+playerInput=prompt('Wybierz swój ruch! 1: kamień, 2:papier, 3:nożyce.');
+console.log('Wpisana odpowiedź to:'+ playerInput);
+
+if (playerInput=='1'){
+    playerMove='kamień';
+} else if (playerInput=='2'){
+    playerMove='kpapier';
+}else if (playerInput=='3'){
+    playerMove='nożyce';
+}
+else{
+    playerMove='nieznany ruch';
+}
+
+printMessage(Twój ruch:' + playerMove');
